6721f7c7e76daf00fe7beccb97ce5a0f9dc13f71
ECHOOOH <hzq200524@163.com>
2021-12-05 14:05:09 +0800

    delete md

---
+++ ch2/cmake-build-debug/CMakeFiles/useHello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/useHello"
+  "../bin/useHello.pdb"
+  "CMakeFiles/useHello.dir/src/useHello.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/useHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
